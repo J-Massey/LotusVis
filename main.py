99107ab1051df5462066a15dca89650808ed0199
J-Massey <masseyjmo@gmail.com>
2022-02-02 14:22:15 +0000

    Testing venv

--- main.py
+++ main.py
@@ -8,25 +8,19 @@ from lotusvis.plot_flow import *
 from lotusvis.flow_field import *
 
 
-# def fluid_vis(sim_dir, length_scale):
-#     os.chdir(sim_dir)
-#     print(os.getcwd(), length_scale)
-#     os.system('mkdir -p vis_dump')
-#     snap = base.FileHandler(os.path.join(sim_dir), 'fluid', length_scale)
-#     flow = base.FlowBase(snap.snap, slice=True)
-#     print(np.shape(flow.x), np.shape(flow.u))
-#     save_figure_to = os.path.join(sim_dir, 'vis_dump/test.png')
-#     plotter.plot_fill(flow.x, flow.y, flow.x, save_figure_to,
-#                       title='Test new plot method', lims=[0, 1.4], levels=101)
+def fluid_vis(sim_dir, length_scale):
+    os.chdir(sim_dir)
+    print(os.getcwd(), length_scale)
+    os.system('mkdir -p vis_dump')
+    plot = Plots(data_root, 'fluid', length_scale=length_scale)
+    plot.plot_vort(os.path.join(data_root, 'vis_dump/vort.png'))
+    plot.plot_mag(os.path.join(data_root, 'vis_dump/mag.png'))
+    plot.plot_pressure(os.path.join(data_root, 'vis_dump/pressure.png'))
 
 
 if __name__ == "__main__":
     plt.style.use(['science', 'grid'])
     data_root = '/run/user/1000/gvfs/sftp:host=iridis5_d.soton.ac.uk,user=jmom1n15/' \
                 'scratch/jmom1n15/Lotus/swimming_plate/rough/res_test/batch0/512'
-    # data_root = sys.argv[1]
-    # length_scale = sys.argv[2]
-    plot = Plots(data_root, 'fluid', length_scale=512)
-    plot.plot_vort(os.path.join(data_root, 'vis_dump/vort.png'))
-    plot.plot_mag(os.path.join(data_root, 'vis_dump/mag.png'))
-    plot.plot_pressure(os.path.join(data_root, 'vis_dump/pressure.png'))
+    fluid_vis(data_root, 512)
+
