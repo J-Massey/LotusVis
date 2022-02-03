d7823608f9cd6e5388a64785f3d99bc76d63252d
J-Massey <masseyjmo@gmail.com>
2022-02-03 12:44:58 +0000

    Got rid of arguments and set it up so we can add the function in main.py to the run.py simulation script

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ from lotusvis.plot_flow import *
 from lotusvis.flow_field import *
 
 
-def fluid_vis():
-    try:
-        sim_dir = str(sys.argv[2])
-    except IndexError:
-        sim_dir = os.getcwd()
-    length_scale = int(sys.argv[1])
+def fluid_vis(length_scale):
+    sim_dir = os.getcwd()
     os.chdir(sim_dir)
     os.system('mkdir -p vis_dump')
-    plot = Plots(sim_dir, 'fluid', length_scale=length_scale)
-    plot.plot_vort(os.path.join(sim_dir, 'vis_dump/vort.png'))
-    plot.plot_mag(os.path.join(sim_dir, 'vis_dump/mag.png'))
-    plot.plot_pressure(os.path.join(sim_dir, 'vis_dump/pressure.png'))
+    try:
+        plot = Plots(sim_dir, 'fluid', length_scale=length_scale)
+        plot.plot_vort(os.path.join(sim_dir, 'vis_dump/vort.png'))
+        plot.plot_mag(os.path.join(sim_dir, 'vis_dump/mag.png'))
+        plot.plot_pressure(os.path.join(sim_dir, 'vis_dump/pressure.png'))
+    except FileNotFoundError:
+        print('Simulation probably failed')
 
 
 if __name__ == "__main__":
-    fluid_vis()
+    c = 512
+    fluid_vis(c)
 
