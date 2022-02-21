904fd23eb492cb03972c39522b4766b140776f6e
masseyjmo <masseyjmo@gmail.com>
2022-02-21 11:18:03 +0000

    Switched from vtr-vti

--- lotusvis/io.py
+++ lotusvis/io.py
@@ -60,12 +60,11 @@ def read_vti(file):
     sca = np.array(pointData.GetScalars('Pressure')).reshape(sh + (1,))
 
     # Generate grid
-    # get grid
-    x = np.array(data.GetXCoordinates())
-    y = np.array(data.GetYCoordinates())
-    z = np.array(data.GetZCoordinates())
+    # nPoints = dat.GetNumberOfPoints()
+    (xmin, xmax, ymin, ymax, zmin, zmax) = data.GetBounds()
+    grid3D = np.mgrid[xmin:xmax + 1, ymin:ymax + 1, zmin:zmax + 1]
 
-    return np.transpose(np.array(vec), (0, 3, 2, 1)), np.transpose(sca, (0, 3, 2, 1)), np.array((x, y, z))
+    return np.transpose(np.array(vec), (0, 3, 2, 1)), np.transpose(sca, (0, 3, 2, 1)), grid3D
 
 
 def format_2d(fn, length_scale, rotation=0):
@@ -85,7 +84,39 @@ def format_2d(fn, length_scale, rotation=0):
 
     """
     rot = rotation / 180 * np.pi
-    data = read_vti(fn)
+    data = read_vtr(fn)
+    # Get the grid
+    X, Y, z = data[2]
+    # X, Y = np.meshgrid(x / length_scale, y / length_scale)
+    # X = np.cos(rot) * X + np.sin(rot) * Y
+    # Y = -np.sin(rot) * X + np.cos(rot) * Y
+
+    u, v, w = data[0]
+    U = np.cos(rot) * u + np.sin(rot) * v
+    V = -np.sin(rot) * u + np.cos(rot) * v
+    p = data[1]
+    p = np.reshape(p, [np.shape(p)[0], np.shape(p)[2], np.shape(p)[3]])
+    return X, Y, U, V, w, p
+
+
+def vtr_format_2d(fn, length_scale, rotation=0):
+    """
+    Rotates and scales vtr file
+    Args:
+        fn: The path to the 'datp' folder
+        length_scale: length scale of the simulation
+        rotation: Rotate the grid. If you're running a simulation with
+                  an angle of attack, it's better to rotate the flow than
+                  the foil because of the meshing.
+
+    Returns: X, Y - coordinates (useful for indexing)
+             U, V - rotated velocity components
+             w    - un-rotated z velocity component
+             p    - pressure field
+
+    """
+    rot = rotation / 180 * np.pi
+    data = read_vtr(fn)
     # Get the grid
     x, y, z = data[2]
     X, Y = np.meshgrid(x / length_scale, y / length_scale)
