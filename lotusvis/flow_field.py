c14be4a5c216dff667f687eaf8a8156ca62aa996
masseyjmo <masseyjmo@gmail.com>
2022-04-06 13:10:13 +0100

    Added some timings to the extraction method, and functionality for Time Averaging

--- lotusvis/flow_field.py
+++ lotusvis/flow_field.py
@@ -7,6 +7,7 @@
 
 # Imports
 import os
+import time
 import numpy as np
 import lotusvis.io as io
 from tkinter import Tcl
@@ -19,16 +20,22 @@ class FlowBase:
     """
 
     def __init__(self, sim_dir, fn_root, length_scale, ext='vti', **kwargs):
+        time_start = time.process_time()
         self.sim_dir = sim_dir
         self.datp_dir = os.path.join(sim_dir, 'datp')
         self.length_scale = length_scale
 
         print(f'Looking for .p{ext} files')
+        time_read = time.process_time()
         fns = [fn for fn in os.listdir(self.datp_dir) if fn.startswith(fn_root) and fn.endswith(f'.p{ext}')]
         self.fns = Tcl().call('lsort', '-dict', fns)
+        print(f'Found {len(self.fns)} instances in {time.process_time()-time_read}s, now extracting data and '
+              f'assigning properties')
 
         self.X, self.Y, self.U, self.V, self.p = None, None, None, None, None
         t_avg = kwargs.get('t_avg', False)
+
+        time_prop = time.process_time()
 
         if len(fns) > 1:
             if t_avg:
@@ -36,10 +43,12 @@ class FlowBase:
             else:
                 props = self.single_instance(ext)
         else:
-            assert (len(fns) > 0), f'You dont have {fn_root}.p{ext} in your {self.datp_dir} folder'
+            assert (len(fns) > 0), f"You don't have {fn_root}.p{ext} in your {self.datp_dir} folder"
             props = self.single_instance(ext)
 
         self.assign_props(props)
+        print(f"Extracted data, and assigned properties in {time.process_time()-time_prop}s")
+        print(f"Finished finding and extracting .p{ext} files in {time.process_time()-time_start}s")
 
     def assign_props(self, snap):
         self.X, self.Y = snap[0:2]
@@ -57,13 +66,13 @@ class FlowBase:
         return snap
 
     def time_avg(self, ext):
-        snaps = []
-        for fn in self.fns:
+        snaps = np.zeros(len(self.fns))
+        for idx, fn in enumerate(self.fns):
             if ext == 'vti':
-                snap = io.vti_format_2d(os.path.join(self.datp_dir, self.fns[-1]), self.length_scale)
+                snap = io.vti_format_2d(os.path.join(self.datp_dir, fn), self.length_scale)
             else:
                 snap = io.vtr_format_2d(os.path.join(self.datp_dir, self.fns[-1]), self.length_scale)
-            snaps.append(snap)
+            snaps[idx] = snap
         del snap
         # Time average the flow field snaps
         mean_t = np.mean(np.array(snaps).T, axis=1)
