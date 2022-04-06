Bug in time average

lotusvis/io.py
@@ -68,7 +68,7 @@ def read_vti(file):
 
     grid = generate_grid(np.shape(U), data)
 
-    return (U, V, W), p, grid
+    return (U, V, W), p.T, grid
 
 
 def generate_grid(shape, data):